174f3a8fe2b15a0d482e8f42ec7cd603c35995fa
David Kua <david@kua.io>
2014-02-05 17:24:18 -0500

    Created two Display functions.
    Display() will now fit within Twitter's char limit.
    PrettyDisplay() looks better but doesn't fit.

--- sudoku.go
+++ sudoku.go
@@ -226,6 +226,18 @@ func clone(values map[string]string) map[string]string {
 // Return this map of values as a 2-D grid string.
 func Display(values map[string]string) string {
 	var buffer bytes.Buffer
+	for _, row := range rows {
+		for _, col := range cols {
+			buffer.WriteString(fmt.Sprintf("%v", values[string(row)+string(col)]))
+		}
+		buffer.WriteString("\n")
+	}
+	return buffer.String()
+}
+
+// Return this map of values as a formatted 2-D grid string.
+func PrettyDisplay(values map[string]string) string {
+	var buffer bytes.Buffer
 	for r, row := range rows {
 		for c, col := range cols {
 			if (c == 3) || (c == 6) {
